ec830c552b77b9f46e42bb9988defc51834f1293
a1234 <manap.aminov@gmail.com>
2026-09-23 22:43:04 +0300

    back to return headers and body

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"time"
@@ -99,8 +100,20 @@ func main() {
 			fmt.Println("Первым ответил: ", currentResult.url)
 			fmt.Println("Status: ", currentResult.resp.Status)
 
-			cancel()
+			for key, values := range currentResult.resp.Header {
+				for _, value := range values {
+					fmt.Printf("%s: %s\n", key, value)
+				}
+			}
 
+			fmt.Println()
+
+			_, err := io.Copy(os.Stdout, currentResult.resp.Body)
+			if err != nil {
+				fmt.Println("Ошибка при чтении body: ", err)
+			}
+
+			cancel()
 			currentResult.resp.Body.Close()
 			return
 
