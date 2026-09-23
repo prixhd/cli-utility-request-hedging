b5b19ea42200bdd7440249ad21393156ed5509eb
a1234 <manap.aminov@gmail.com>
2026-09-23 16:19:20 +0300

    add gorutines to my project

--- main.go
+++ main.go
@@ -4,29 +4,41 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 )
 
+func getStatusToUrl(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	resp, err := http.Get(url)
+
+	// Обработка ошибки
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("URL: ", url)
+	fmt.Println("Status: ", resp.Status)
+
+	for key, values := range resp.Header {
+		for _, value := range values {
+			fmt.Printf("%s: %s\n", key, value)
+		}
+	}
+
+}
+
 func main() {
+	var wg sync.WaitGroup
 	urls := os.Args[1:]
+	wg.Add(len(urls))
 
 	for _, url := range urls {
+		go getStatusToUrl(url, &wg)
 
-		resp, err := http.Get(url)
-
-		// Обработка ошибки
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		fmt.Println("Status:", resp.Status)
-
-		for key, values := range resp.Header {
-			for _, value := range values {
-				fmt.Printf("%s: %s\n", key, value)
-			}
-		}
-
-		resp.Body.Close()
 	}
+
+	wg.Wait()
 }
